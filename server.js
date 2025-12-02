54185777bf51d7aede0e7addfbf8656421d0a927
rii381 <riight00028@gmail.com>
2025-12-02 18:08:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,46 +9,41 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// データの管理
 let playlist = []; 
 let currentSongIndex = 0;
 let isPlaying = false;
-let startTime = 0;
 
 io.on('connection', (socket) => {
+    // 1. 接続時に今の状態を教える
     socket.emit('init_state', {
         playlist,
         currentSongIndex,
-        isPlaying,
-        elapsedTime: isPlaying ? (Date.now() - startTime) / 1000 : 0
+        isPlaying
     });
 
+    // 2. 曲の追加
     socket.on('add_song', (videoId) => {
-        const song = { id: videoId };
-        playlist.push(song);
+        playlist.push({ id: videoId });
         io.emit('update_playlist', playlist);
-        if (playlist.length === 1 && !isPlaying) {
-            playSong(0);
+        // 最初の1曲目なら自動でセット
+        if (playlist.length === 1) {
+            currentSongIndex = 0;
+            io.emit('change_song', { index: 0, videoId: videoId });
         }
     });
 
-    socket.on('song_ended', () => {
-        if (currentSongIndex + 1 < playlist.length) {
-            playSong(currentSongIndex + 1);
-        } else {
-            isPlaying = false;
-            io.emit('stop_player');
-        }
-    });
-});
-
-function playSong(index) {
-    currentSongIndex = index;
-    isPlaying = true;
-    startTime = Date.now();
-    io.emit('play_song', { videoId: playlist[index].id, index: index });
-}
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Start! http://localhost:${PORT}`);
-});
+    // 3. 再生・一時停止・スキップ
+    socket.on('control', (action) => {
+        switch(action.type) {
+            case 'play':
+                isPlaying = true;
+                io.emit('sync_action', { type: 'play' });
+                break;
+            case 'pause':
+                isPlaying = false;
+                io.emit('sync_action', { type: 'pause' });
+                break;
+            case 'next':
+                if (currentSongIndex + 1 < playlist.length) {
+                    current
